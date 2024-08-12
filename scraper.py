97af1b336a4251a466165fc62ff4015aad160399
junliaw <junliaw@gmail.com>
2024-08-12 17:18:38 +0800

    3.4批次爬取子網頁

--- scraper.py
+++ scraper.py
@@ -10,4 +10,13 @@ title_list=[]
 for title in titles:
     title_list.append(title.getText())
 
-print(title_list)
+tag_list=[]
+urls=soup.find_all('a',{'class' :'focusIndicatorDisplayBlock bbc-uk8dsi e1d658bg0'})
+for url in urls:
+    sub_response=requests.get(url.get('href'))
+    sub_soup=BeautifulSoup(sub_response.text,'lxml')
+    tags=sub_soup.find_all('li',{'class':'bbc-1ufuo2s e2o6ii40'})
+    for tag in tags:
+        tag_list.append(tag.getText())
+
+print(tag_list)
